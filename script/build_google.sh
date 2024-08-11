e8a3786327e8d13ac74b7675c2c746ecc932288d
Yaroslav51-dev <ixityzv@gmail.com>
2024-08-12 00:00:27 +0300

    Update build_google.sh

--- script/build_google.sh
+++ script/build_google.sh
@@ -26,6 +26,6 @@ LLVM_IAS=1
 DTC_EXT=dtc
 "
 
-make -j$(nproc) -C O=out ${ARGS} clean && mkdir out &&make -j8 -C O=/out ${ARGS} mrproper
+make -j$(nproc) -C O=out ${ARGS} clean && mkdir /out && make -j8 -C O=out ${ARGS} mrproper
 make -j$(nproc) -C O=out ${ARGS} vayu_user_defconfig
 make -j$(nproc) -C O=out ${ARGS}
