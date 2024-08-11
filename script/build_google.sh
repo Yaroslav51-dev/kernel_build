729108820d387b865a8ee784c4be8771622ad714
Yaroslav51-dev <ixityzv@gmail.com>
2024-08-12 00:45:41 +0300

    Update build_google.sh

--- script/build_google.sh
+++ script/build_google.sh
@@ -4,27 +4,7 @@ cd $HOME/vayu/
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 
-ARGS="
-CC=clang
-CROSS_COMPILE=aarch64-linux-gnu-
-ARCH=arm64
-LD=ld.lld
-AR=llvm-ar
-NM=llvm-nm
-OBJCOPY=llvm-objcopy
-OBJDUMP=llvm-objdump
-READELF=llvm-readelf
-OBJSIZE=llvm-size
-STRIP=llvm-strip
-LLVM_AR=llvm-ar
-LLVM_DIS=llvm-dis
-CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-ARCH=arm64
-SUBARCH=arm64
-LLVM=1
-LLVM_IAS=1
-DTC_EXT=dtc
-"
+ARGS="CC=clang CROSS_COMPILE=aarch64-linux-gnu- ARCH=arm64 LD=ld.lld AR=llvm-ar NM=llvm-nmOBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf OBJSIZE=llvm-size STRIP=llvm-strip LLVM_AR=llvm-ar LLVM_DIS=llvm-dis CROSS_COMPILE_ARM32=arm-linux-gnueabi- SUBARCH=arm64 LLVM=1 LLVM_IAS=1 DTC_EXT=dtc"
 
 make -j$(nproc) -C ${ARGS} 0=out mrproper
 make -j$(nproc) -C ${ARGS} 0=out vayu_user_defconfig
