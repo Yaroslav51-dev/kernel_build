3869610d0bd477e5c22078664066ef19e3db649f
Yaroslav51-dev <ixityzv@gmail.com>
2024-12-07 00:25:08 +0800

    Update build_google.sh

--- script/build_google.sh
+++ script/build_google.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd $HOME/kernel/
 curl https://raw.githubusercontent.com/akhilnarang/scripts/master/setup/android_build_env.sh | bash -
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 
-make -s ARCH=arm64 O=out liuqin_defconfig -j$(nproc --all)
-make -j$(nproc --all) O=out DTC_EXT=dtc ARCH=arm64 CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu-
+make mrproper
+make -s ARCH=arm64 O=out nabu-stock_defconfig -j$(nproc --all) SUBARCH=arm64
+make -j$(nproc --all) O=out DTC_EXT=dtc ARCH=arm64 REAL_CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
