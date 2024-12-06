84837f2c19d31776203c69665dada3520b004291
Yaroslav51-dev <ixityzv@gmail.com>
2024-12-07 00:19:57 +0800

    Update build_weebx.sh

--- script/build_weebx.sh
+++ script/build_weebx.sh
@@ -14,6 +14,6 @@ export KBUILD_BUILD_USER=t.me
 export PATH="$HOME/gcc/bin:$PATH"
 
 make mrproper
-make -s ARCH=arm64 O=out vayu_user_defconfig -j$(nproc --all) SUBARCH=arm64
+make -s ARCH=arm64 O=out nabu-stock_defconfig -j$(nproc --all) SUBARCH=arm64
 make -j$(nproc --all) O=out DTC_EXT=dtc ARCH=arm64 REAL_CC=clang CLANG_TRIPLE=aarch64-linux-gnu- CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
